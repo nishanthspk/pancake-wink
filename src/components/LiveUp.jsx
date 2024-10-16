1aecde3a01c44cd91bcbb22a4226e6cce37c0055
nishanthspk <spknishanth2004@gmail.com>
2024-10-16 16:11:49 +0530

    text

--- src/components/LiveUp.jsx
+++ src/components/LiveUp.jsx
@@ -9,9 +9,9 @@ import RoundProgress from "./RoundProgress";
 import { useAccount } from 'wagmi'
 
 const BetPosition = {
-  BULL : 'Bull',
-  BEAR : 'Bear',
-  HOUSE : 'House',
+  BULL: 'Bull',
+  BEAR: 'Bear',
+  HOUSE: 'House',
 }
 const LiveUp = ({ handleFlip }) => {
   const [provider, setProvider] = useState(null);
@@ -33,9 +33,9 @@ const LiveUp = ({ handleFlip }) => {
   const [realprice, setRealprice] = useState("")
   const { data, refetch, isFetching, error } = usePriceData();
 
-  const account  = useAccount()
+  const account = useAccount()
 
-  const config ={
+  const config = {
     "address": "0x18B2A687610328590Bc8F2e5fEdDe3b582A49cdA",
     "api": "https://thegraph.pancakeswap.com/prediction-v2-bsc",
     "chainlinkOracleAddress": "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE",
@@ -65,7 +65,7 @@ const LiveUp = ({ handleFlip }) => {
     // Format price to have a decimal point, e.g., 504.30606593
     if (isNaN(priceNum)) return 'No data available';
     return (priceNum / 1e8).toFixed(4); // Assuming the data is in nanounits and needs division by 1e9
-};
+  };
   const formatPrice2 = (price) => {
     // Convert price to a number if it's not already
     const priceNum = Number(price);
@@ -73,137 +73,137 @@ const LiveUp = ({ handleFlip }) => {
     // Format price to have a decimal point, e.g., 504.30606593
     if (isNaN(priceNum)) return 'No data available';
     return (priceNum / 1e9).toFixed(3); // Assuming the data is in nanounits and needs division by 1e9
-};
+  };
 
-const formatPrice3 = (price) => {
-  // Convert price to a number if it's not already
-  const priceNum = Number(price);
+  const formatPrice3 = (price) => {
+    // Convert price to a number if it's not already
+    const priceNum = Number(price);
 
-  // Format price to have a decimal point, e.g., 504.30606593
-  if (isNaN(priceNum)) return 'No data available';
-  return (priceNum / 1e18).toFixed(4); // Assuming the data is in nanounits and needs division by 1e9
-};
+    // Format price to have a decimal point, e.g., 504.30606593
+    if (isNaN(priceNum)) return 'No data available';
+    return (priceNum / 1e18).toFixed(4); // Assuming the data is in nanounits and needs division by 1e9
+  };
 
 
-const getMultiplierV2 = (total, amount) => {
-  if (!total) {
-    return 0
+  const getMultiplierV2 = (total, amount) => {
+    if (!total) {
+      return 0
+    }
+
+    if (total === 0n || amount === 0n) {
+      return 0
+    }
+
+    const rewardAmountFixed = new BN(total.toString())
+    const multiplierAmountFixed = new BN(amount.toString())
+
+    return rewardAmountFixed.div(multiplierAmountFixed)
   }
 
-  if (total === 0n || amount === 0n) {
-    return 0
+
+  const REFRESH_PRICE_BEFORE_SECONDS_TO_CLOSE = 2
+
+  const SHOW_HOUSE_BEFORE_SECONDS_TO_CLOSE = 20
+  const [isCalculatingPhase, setIsCalculatingPhase] = useState(false)
+
+  const getPriceDifference = (price, lockPrice) => {
+
+
+    if (!price || !lockPrice) {
+      return 0
+    }
+
+    return price - lockPrice
   }
+  const [closeTimestamp, setCloseTimestamp] = useState()
 
-  const rewardAmountFixed = new BN(total.toString())
-  const multiplierAmountFixed = new BN(amount.toString())
+  const [lockTimestamp, setLockTimestamp] = useState()
+  const reff1 = setTimeout(() => {
+    refresh()
+  }, (REFRESH_PRICE_BEFORE_SECONDS_TO_CLOSE) * 5000)
 
-  return rewardAmountFixed.div(multiplierAmountFixed)
-}
+
+  async function initialize() {
+    try {
 
 
-const REFRESH_PRICE_BEFORE_SECONDS_TO_CLOSE = 2
+      const { provider, signer, contract } =
+        await initializeContract();
+      setProvider(provider);
+      setSigner(signer);
+      setContract(contract);
 
-const SHOW_HOUSE_BEFORE_SECONDS_TO_CLOSE = 20
-const [isCalculatingPhase, setIsCalculatingPhase] = useState(false)
+      const epoch = await contract.currentEpoch();
+      setActualEpoch(Number(epoch.toString())); // Convert and set for display
+      const epoch_in_string = epoch.toString();
 
-const getPriceDifference = (price , lockPrice) => {
+      const rounds = await contract.rounds(Number(epoch_in_string) - 1);
 
- 
-  if (!price || !lockPrice) {
-    return 0
+
+      const totslval = rounds["lockPrice"];
+
+      const pricepool = rounds["totalAmount"];
+
+      const ba = rounds["bullAmount"];
+
+      const bearamoint = rounds["bearAmount"];
+
+      setLockpriceActual(totslval)
+
+      const closeTimestamp = rounds["closeTimestamp"];
+      const lockTimestamp = rounds["lockTimestamp"];
+
+      setCloseTimestamp(closeTimestamp)
+      setLockTimestamp(lockTimestamp)
+
+      const r = formatPrice3(pricepool.toString())
+      seTpricepool(r);
+
+      const val = formatPrice(totslval.toString())
+
+
+      setLockPrice(val)
+
+
+
+      const bullMultiplier = pricepool && ba ? getMultiplierV2(pricepool, ba) : 0
+      const bearMultiplier = pricepool && bearamoint ? getMultiplierV2(pricepool, bearamoint) : 0
+
+
+      const formattedBullMultiplier = bullMultiplier.toFixed(bullMultiplier.isZero() ? 0 : 2)
+      const formattedBearMultiplier = bearMultiplier.toFixed(bearMultiplier.isZero() ? 0 : 2)
+
+
+
+
+
+      setupval(formattedBullMultiplier)
+
+      setDownVal(formattedBearMultiplier)
+
+
+
+    } catch (error) {
+      console.error("Error initializing contract:", error);
+    }
   }
-
-  return price - lockPrice
-}
-const [closeTimestamp, setCloseTimestamp] = useState()
-
-const [lockTimestamp, setLockTimestamp] = useState()
-const reff1 = setTimeout(() => {
-  refresh()
-}, (REFRESH_PRICE_BEFORE_SECONDS_TO_CLOSE) * 5000)
-
-
-async function initialize() {
-  try {
-
-   
-    const { provider, signer, contract } =
-      await initializeContract();
-    setProvider(provider);
-    setSigner(signer);
-    setContract(contract);
-
-    const epoch = await contract.currentEpoch();
-    setActualEpoch(Number(epoch.toString())); // Convert and set for display
-    const epoch_in_string= epoch.toString();
-
-    const rounds = await contract.rounds(Number(epoch_in_string)-1);
-
-
-    const totslval= rounds["lockPrice"];
-
-    const pricepool= rounds["totalAmount"];
-
-    const ba = rounds["bullAmount"];
-
-    const bearamoint = rounds["bearAmount"];
-
-setLockpriceActual(totslval)
-
-    const closeTimestamp = rounds["closeTimestamp"];
-    const lockTimestamp = rounds["lockTimestamp"];
-
-    setCloseTimestamp(closeTimestamp)
-setLockTimestamp(lockTimestamp)
-
-    const r = formatPrice3(pricepool.toString())
-seTpricepool(r);
-
-    const  val = formatPrice(totslval.toString())
-
-
-    setLockPrice(val)
-
-
-
-const bullMultiplier = pricepool && ba ? getMultiplierV2(pricepool, ba) : 0
-const bearMultiplier = pricepool && bearamoint ? getMultiplierV2(pricepool, bearamoint) : 0
-
-
-const formattedBullMultiplier = bullMultiplier.toFixed(bullMultiplier.isZero() ? 0 : 2)
-const formattedBearMultiplier = bearMultiplier.toFixed(bearMultiplier.isZero() ? 0 : 2)
-
-
-
-
-
-setupval(formattedBullMultiplier)
-
-setDownVal(formattedBearMultiplier)
-
-
-
-  } catch (error) {
-    console.error("Error initializing contract:", error);
-  }
-}
 
   useEffect(() => {
 
     initialize();
-  },[]);
+  }, []);
   const getNowInSeconds = () => Math.floor(Date.now() / 1000)
 
   const isHouse = useMemo(() => {
     const secondsToClose = closeTimestamp ? closeTimestamp - getNowInSeconds() : 0
     return Boolean(lockpriceActual && price === lockpriceActual && secondsToClose <= SHOW_HOUSE_BEFORE_SECONDS_TO_CLOSE)
   }, [closeTimestamp, lockPrice, price])
-  
+
   const isBull = Boolean(lockPrice && price > lockPrice)
-  
+
   const betPosition = isHouse ? BetPosition.HOUSE : isBull ? BetPosition.BULL : BetPosition.BEAR
 
-  
+
   const priceDifference = getPriceDifference(Number(price), lockpriceActual ?? 0)
   // const hasRoundFailed = getHasRoundFailed(round.oracleCalled, round.closeTimestamp, bufferSeconds, round.closePrice)
   useEffect(() => {
@@ -227,8 +227,8 @@ setDownVal(formattedBearMultiplier)
 
   const reff2 = setTimeout(() => {
     initialize()
-  }, 10* 1000)
-  
+  }, 10 * 1000)
+
 
   const startMs = lockTimestamp * 1000
   const endMs = closeTimestamp * 1000
@@ -237,117 +237,117 @@ setDownVal(formattedBearMultiplier)
   const progress = rawProgress <= 100 ? rawProgress : 100
   const refValTimeour = setTimeout(() => {
 
-  if(Math.round(progress) == 100) {
-    initialize()
-  }
+    if (Math.round(progress) == 100) {
+      initialize()
+    }
 
-  }, ( REFRESH_PRICE_BEFORE_SECONDS_TO_CLOSE) * 1000)
+  }, (REFRESH_PRICE_BEFORE_SECONDS_TO_CLOSE) * 1000)
 
 
-  const bg1 = formatPrice(priceDifference) >=0 ? "bg-[#31D0AA] text-[#353547]" : "bg-[#353547] text-white "
-  const bg2 = formatPrice(priceDifference) >0 ? "bg-[#353547] text-white":   "bg-[#31D0AA] text-white " 
+  const bg1 = formatPrice(priceDifference) >= 0 ? "bg-[#31D0AA] text-[#353547]" : "bg-[#353547] text-white "
+  const bg2 = formatPrice(priceDifference) > 0 ? "bg-[#353547] text-white" : "bg-[#31D0AA] text-white "
 
   return (
     <>
-   <div className="flex justify-center items-center pt-10 text-white">
-    <div className="bg-[#27262C] shadow-xl lg:h-full rounded-3xl md:mx-0 w-[180px] md:w-[240px]">
-      <div className="bg-tranparent text-white font-bold p-2 px-4 flex justify-between items-center rounded-t-4xl  border-[#A881FC]">
+      <div className="flex justify-center items-center pt-10 text-white">
+        <div className="bg-[#27262C] shadow-xl lg:h-full rounded-3xl md:mx-0 w-[180px] md:w-[240px]">
+          <div className="bg-tranparent text-white font-bold p-2 px-4 flex justify-between items-center rounded-t-4xl  border-[#A881FC]">
 
-     
-        <div className="flex items-center text-[#A881FC] text-base gap-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="size-5"
-          >
-            <path
-              fillRule="evenodd"
-              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <p>LIVE</p>
-        </div>
-        <div className="text-sm text-[#A881FC]"># {Number(actualEpoch) -1}</div>
-      </div>
-       <RoundProgress
-    variant="flat"
-    scale="sm"
-    lockTimestamp={lockTimestamp ?? 0}
-    closeTimestamp={closeTimestamp ?? 0}
-  />
-      <div className="p-4">
-        <div className={`font-bold text-center p-1 ${bg1} w-[140px] rounded-t-xl mx-auto`}>
-          <p className="text-base font-extrabold">UP</p>
-          <p className="text-sm">
-            {upval}x <span className=" font-medium"> payout</span>
-          </p>
-        </div>
-        <div className="w-full mx-auto border-2 border-[#31D0AA] p-4 rounded-xl">
-          <div className="flex justify-between font-bold">
-            <p className=" text-[#B0A5C9] text-sm">LAST PRICE</p>
-          </div>
 
-          <div className="flex justify-between items-center">
-            <p className=" font-bold text-[#31D0AA] text-base px-1">${formatPrice(price)}</p>
-
-            {/* <p className=" font-bold text-[#31D0AA] text-base">${formatPrice(realprice)}</p> */}
-         {
-          formatPrice(priceDifference) >= 0 ?   <div className=" flex items-center bg-[#31D0AA] p-1 px-2 rounded-lg font-semibold text-xs">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M11.47 2.47a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06l-6.22-6.22V21a.75.75 0 0 1-1.5 0V4.81l-6.22 6.22a.75.75 0 1 1-1.06-1.06l7.5-7.5Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-   ${formatPrice(priceDifference)}
-        </div> : <div className=" flex items-center bg-[#ED4B9E] p-1 px-2 rounded-lg font-semibold text-xs">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M11.47 2.47a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06l-6.22-6.22V21a.75.75 0 0 1-1.5 0V4.81l-6.22 6.22a.75.75 0 1 1-1.06-1.06l7.5-7.5Z"
-              clip-rule="evenodd"
-
-               transform="rotate(180 12 12)"
-            />
-          </svg>
-   ${formatPrice(priceDifference)}
-        </div>
-         }
-      
-          </div>
-          <div className="pt-2">
-            <div className=" flex justify-between items-center text-xs">
-                <p>Locked Price: </p>
-                <p>${lockPrice}</p>
+            <div className="flex items-center text-[#A881FC] text-base gap-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <p>LIVE</p>
             </div>
-            <div className=" flex justify-between items-center font-bold text-sm">
-                <p>Price Pool: </p>
-                <p>{pricepool} BNB</p>
+            <div className="text-sm text-[#A881FC]"># {Number(actualEpoch) - 1}</div>
+          </div>
+          <RoundProgress
+            variant="flat"
+            scale="sm"
+            lockTimestamp={lockTimestamp ?? 0}
+            closeTimestamp={closeTimestamp ?? 0}
+          />
+          <div className="p-4">
+            <div className={`font-bold text-center p-1 ${bg1} w-[140px] rounded-t-xl mx-auto`}>
+              <p className="text-base font-extrabold">UP</p>
+              <p className="text-sm">
+                {upval}x <span className=" font-medium"> payout</span>
+              </p>
+            </div>
+            <div className="w-full mx-auto border-2 border-[#31D0AA] p-4 rounded-xl">
+              <div className="flex justify-between font-bold">
+                <p className=" text-[#B0A5C9] text-[10px] lg:text-sm">LAST PRICE</p>
+              </div>
+
+              <div className="flex justify-between items-center">
+                <p className=" font-bold text-[#31D0AA] text-[10px] lg:text-base px-1">${formatPrice(price)}</p>
+
+                {/* <p className=" font-bold text-[#31D0AA] text-base">${formatPrice(realprice)}</p> */}
+                {
+                  formatPrice(priceDifference) >= 0 ? <div className=" flex items-center bg-[#31D0AA] p-1 md:px-2 rounded-lg font-semibold text-[8px] md:text-xs">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="size-5"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M11.47 2.47a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06l-6.22-6.22V21a.75.75 0 0 1-1.5 0V4.81l-6.22 6.22a.75.75 0 1 1-1.06-1.06l7.5-7.5Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    ${formatPrice(priceDifference)}
+                  </div> : <div className=" flex items-center bg-[#ED4B9E] p-1 px-2 rounded-lg font-semibold text-xs">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="size-5"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M11.47 2.47a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06l-6.22-6.22V21a.75.75 0 0 1-1.5 0V4.81l-6.22 6.22a.75.75 0 1 1-1.06-1.06l7.5-7.5Z"
+                        clip-rule="evenodd"
+
+                        transform="rotate(180 12 12)"
+                      />
+                    </svg>
+                    ${formatPrice(priceDifference)}
+                  </div>
+                }
+
+              </div>
+              <div className="pt-2">
+                <div className=" flex justify-between items-center text-[10px] md:text-xs">
+                  <p>Locked Price: </p>
+                  <p>${lockPrice}</p>
+                </div>
+                <div className=" flex justify-between items-center font-bold text-sm">
+                  <p>Price Pool: </p>
+                  <p>{pricepool} BNB</p>
+                </div>
+              </div>
+            </div>
+            <div className={`font-bold text-center p-1 ${bg2} w-[140px] rounded-b-xl mx-auto`}>
+              <p className=" text-sm">
+                {downVal}x <span className=" font-medium"> payout</span>
+              </p>
+              <p className=" text-base">DOWN</p>
             </div>
           </div>
         </div>
-        <div className={`font-bold text-center p-1 ${bg2} w-[140px] rounded-b-xl mx-auto`}>
-          <p className=" text-sm">
-       {downVal}x <span className=" font-medium"> payout</span>
-          </p>
-          <p className=" text-base">DOWN</p>
-        </div>
       </div>
-    </div>
-  </div>
     </>
   );
 };
